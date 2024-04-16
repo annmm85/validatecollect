a39a1e9c482b39723150c7cdd3baa5c6cec8d558
AnnMm85 <8mona854@gmail.com>
2024-04-16 14:20:12 +0700

    rar

--- src/helpers.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Validatecollect;
-
-use Validatecollect\src\Validator\AbstractValidator;
-
-function validatecollection(): AbstractValidator
-{
-    return new AbstractValidator();
-}
